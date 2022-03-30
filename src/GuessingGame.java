7a48491354a15d2c7b751517f84ec2430ef29bb3
mfadhili <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@github.com>
2022-03-30 19:22:47 +0300

    Initial commit

---
+++ src/GuessingGame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class GuessingGame extends JFrame {
+
+
+    private JTextField txtGuess;
+    private JButton guessButton;
+    private JLabel lblOutput;
+    private JLabel lblStatement;
+
+    public void checkGuess(){
+        String guessText = txtGuess.getText();
+        String message = "";
+
+    }
+}
